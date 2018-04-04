5d5c302512156dc3d34033775a1385e6f6326e4d
Andrew Hilts <andrew@citizenlab.ca>
2018-04-04 15:26:25 -0400

    improve Wordpress auto-configuration

--- scripts/wp-ami-setup.sh
+++ scripts/wp-ami-setup.sh
@@ -2,10 +2,12 @@
 cd /var/www/html
 wp core install --url="localhost:8080" --title="WordPress" --admin_user="admin" --admin_password="prollynotadmin" --admin_email="admin@email.com"
 wp plugin install "regenerate-thumbnails" "advanced-custom-fields" "wordpress-importer" "acf-qtranslate" "custom-post-type-ui" "qtranslate-x" "raw-html-snippets" "json-rest-api" "w3-total-cache" "https://github.com/andrewhilts/ami-api/archive/master.zip" "https://github.com/PanMan/WP-JSON-API-ACF/archive/master.zip" "https://github.com/hoppinger/advanced-custom-fields-wpcli/archive/master.zip" --activate
-wp plugin deactivate qtranslate-x
 wp plugin activate ami-api
 wp plugin activate WP-JSON-API-ACF
-# wp import /home/advanced-custom-field-export.xml --authors=skip
+# # Fix PHP warning in qTranslate
+patch wp-content/plugins/qtranslate-x/qtranslate_options.php /home/qtranslate_options.patch
+wp eval-file /home/custom-post-types-import.php
+wp import /home/default-content.xml --authors=skip
 cat /home/cors.php >> wp-content/themes/twentyseventeen/functions.php
-# wp post delete $(wp post list --post_type='request-template, jurisdiction, operator-industry, operator-status, operator, operator-service, request-components, identifier, data-bank , links'  --format=ids)
-wp import /home/default-posts.xml --authors=skip
+wp option update permalink_structure '/%year%/%monthnum%/%day%/%postname%/'
+wp option add qtranslate_hide_default_language 0
