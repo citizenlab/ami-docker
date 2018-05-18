90a3c13e70f640f781e346aec481a5fa172c824e
Andrew Hilts <andrewchilts@gmail.com>
2018-05-18 15:07:17 -0400

    update wp install script to fix the json-rest-api version to one that works

--- scripts/wp-ami-setup.sh
+++ scripts/wp-ami-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/html
 wp core install --url="localhost:8080" --title="WordPress" --admin_user="admin" --admin_password="prollynotadmin" --admin_email="admin@email.com"
-wp plugin install "regenerate-thumbnails" "advanced-custom-fields" "wordpress-importer" "acf-qtranslate" "custom-post-type-ui" "raw-html-snippets" "json-rest-api" "w3-total-cache" "https://github.com/citizenlab/ami-api/archive/master.zip" "https://github.com/PanMan/WP-JSON-API-ACF/archive/master.zip" "https://github.com/hoppinger/advanced-custom-fields-wpcli/archive/master.zip" --activate
+wp plugin install "regenerate-thumbnails" "advanced-custom-fields" "wordpress-importer" "acf-qtranslate" "custom-post-type-ui" "raw-html-snippets" "https://github.com/WP-API/WP-API/archive/1.2.5.zip" "w3-total-cache" "https://github.com/citizenlab/ami-api/archive/master.zip" "https://github.com/PanMan/WP-JSON-API-ACF/archive/master.zip" "https://github.com/hoppinger/advanced-custom-fields-wpcli/archive/master.zip" --activate
 wp plugin install "qtranslate-x"
 wp plugin activate ami-api
 wp plugin activate WP-JSON-API-ACF
